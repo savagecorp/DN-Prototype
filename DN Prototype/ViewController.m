63980f55e7c21c0dc6ea13bd8ed62b25c40d8cdd
Savagecorp <savagecorp@gmail.com>
2014-08-12 00:43:28 -0400

    login button is animating, removed segue to next scene, but can put ti back in with modal > flip

--- DN Prototype/ViewController.m
+++ DN Prototype/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *dialogView;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+- (IBAction)loginButtonDidPress:(id)sender;
 
 @end
 
@@ -20,15 +23,27 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 -(UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
 
 
+- (IBAction)loginButtonDidPress:(id)sender {
+    // Animate Login Button
+    [UIView animateWithDuration:0.1 animations:^{
+        self.loginButton.transform = CGAffineTransformMakeTranslation(10, 0);
+    } completion:^(BOOL finished) {
+        // Step 2
+        [UIView animateWithDuration:0.1 animations:^{
+            self.loginButton.transform = CGAffineTransformMakeTranslation(-10, 0);
+        } completion:^(BOOL finished) {
+            // Step 3
+            [UIView animateWithDuration:0.1 animations:^{
+                self.loginButton.transform = CGAffineTransformMakeTranslation(0, 0);
+            }];
+        }];
+    }];
+    
+}
 @end
