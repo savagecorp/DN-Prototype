33e98639b92efbcdaf6defbb3bb82bd407cb104f
Savagecorp <savagecorp@gmail.com>
2014-08-13 01:02:05 -0400

    textFieldDidEndEditing actions

--- DN Prototype/ViewController.m
+++ DN Prototype/ViewController.m
@@ -8,10 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIView *dialogView;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 - (IBAction)loginButtonDidPress:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *emailImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
 
 @end
 
@@ -21,12 +25,66 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setNeedsStatusBarAppearanceUpdate];
+    
+    self.emailTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    
 }
 
 
 -(UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
+
+// Set up text field deligate stuff
+-(void) textFieldDidBeginEditing:(UITextField *)textField {
+    // Do something when textfield is focused.
+    NSLog(@"%@" , textField);
+    
+    // This is hardCoding position of textFields using frame.origin.y
+    //if (textField.frame.origin.y == 108) {
+    //    NSLog(@"EMAIL");
+    //}
+    
+    //if (textField.frame.origin.y == 162) {
+    //    NSLog(@"Password!");
+    //}
+    
+    //A Better way is to
+    if ([textField isEqual:self.emailTextField]) {
+        [self.emailTextField setBackground:[UIImage imageNamed:@"input-outline-active"]];
+        self.emailImageView.image = [UIImage imageNamed:@"icon-mail-active"];
+    } else {
+        [self.emailTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.emailImageView.image = [UIImage imageNamed:@"icon-mail"];
+    }
+    
+    
+    if ([textField isEqual:self.passwordTextField]) {
+        [self.passwordTextField setBackground:[UIImage imageNamed:@"input-outline-active"]];
+        self.passwordImageView.image = [UIImage imageNamed:@"icon-password-active"];
+    } else {
+        [self.passwordTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.passwordImageView.image = [UIImage imageNamed:@"icon-password"];
+    }
+}
+
+-(void) textFieldDidEndEditing:(UITextField *)textField {
+    if ([textField isEqual:self.emailTextField]) {
+        [self.emailTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.emailImageView.image = [UIImage imageNamed:@"icon-mail"];
+    }
+    
+    
+    if ([textField isEqual:self.passwordTextField]) {
+        [self.passwordTextField setBackground:[UIImage imageNamed:@"input-outline"]];
+        self.passwordImageView.image = [UIImage imageNamed:@"icon-password"];
+    }
+
+}
+
 
 
 - (IBAction)loginButtonDidPress:(id)sender {
@@ -48,7 +106,9 @@
     // animateWithDuration with Damping
     [UIView animateWithDuration:0.7 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:0 animations:^{
         //change size of dialog view
-        [self.dialogView setFrame:CGRectMake(self.dialogView.frame.origin.x, self.dialogView.frame.origin.y, self.dialogView.frame.size.width, 320)];
+        if (self.dialogView.frame.origin.y == 144) {
+            [self.dialogView setFrame:CGRectMake(self.dialogView.frame.origin.x, self.dialogView.frame.origin.y-60, self.dialogView.frame.size.width, 320)];
+        }
     } completion:^(BOOL finished) { }];
     
 }
